79c5b985b7b2d858b5f7ca3a7b3d8f7f57c2587a
Virgilio04 <seu_email@exemplo.com>
2026-01-15 13:09:43 -0300

    adicionando "barra" como opção

--- src/App.jsx
+++ src/App.jsx
@@ -877,6 +877,7 @@ showToast(editingId ? 'Produto atualizado!' : 'Produto adicionado!');
     <option value="un">UN</option>
     <option value="pct">PCT</option>
     <option value="garrafa">GARRAFA</option>
+    <option value="barra">BARRA</option>
     <option value="balde">BALDE</option>
     <option value="lt">LT</option>
     <option value="g">G</option>
